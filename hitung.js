7a1657ea44f22adc0572b39704b23a3c7d096c52
afri111 <gabrieltenma2006@gmail.com>
2025-03-10 15:59:54 +0700

    data

---
+++ hitung.js
@@ -0,0 +1,218 @@
+function hd1() {
+    let val1 = document.getElementById("in1").value;
+    let h1 = val1 * (Math.cos(0) * Math.cos(0));
+    document.getElementById("nin1").value = h1;
+
+
+    let val2 = document.getElementById("in2").value;
+    let h2 = val2 * (Math.cos(20) * Math.cos(20));
+    document.getElementById("nin2").value = h2;
+
+    
+    let val3 = document.getElementById("in3").value;
+    let h3 = val3 * (Math.cos(40) * Math.cos(40));
+    document.getElementById("nin3").value = h3;
+
+
+    let val4 = document.getElementById("in4").value;
+    let h4 = val4 * (Math.cos(60) * Math.cos(60));
+    document.getElementById("nin4").value = h4;
+    
+
+    let val5 = document.getElementById("in5").value;
+    let h5 = val5 * (Math.cos(80) * Math.cos(80));
+    document.getElementById("nin5").value = h5;
+
+
+    let val6 = document.getElementById("in6").value;
+    let h6 = val6 * (Math.cos(100) * Math.cos(100));
+    document.getElementById("nin6").value = h6;
+
+
+    let val7 = document.getElementById("in7").value;
+    let h7 = val7 * (Math.cos(120) * Math.cos(120));
+    document.getElementById("nin7").value = h7;
+
+
+    let val8 = document.getElementById("in8").value;
+    let h8 = val8 * (Math.cos(140) * Math.cos(140));
+    document.getElementById("nin8").value = h8;
+
+
+    let val9 = document.getElementById("in9").value;
+    let h9 = val9 * (Math.cos(160) * Math.cos(160));
+    document.getElementById("nin9").value = h9;
+
+
+    let val0 = document.getElementById("in0").value;
+    let h0 = val9 * (Math.cos(180) * Math.cos(180));
+    document.getElementById("nin0").value = h0;
+
+}
+
+function hd2() {
+    let val1 = document.getElementById("din1").value;
+    let h1 = val1 * (Math.cos(0) * Math.cos(0));
+    document.getElementById("dndin1").value = h1;
+
+
+    let val2 = document.getElementById("din2").value;
+    let h2 = val2 * (Math.cos(20) * Math.cos(20));
+    document.getElementById("dndin2").value = h2;
+
+    
+    let val3 = document.getElementById("din3").value;
+    let h3 = val3 * (Math.cos(40) * Math.cos(40));
+    document.getElementById("dndin3").value = h3;
+
+
+    let val4 = document.getElementById("din4").value;
+    let h4 = val4 * (Math.cos(60) * Math.cos(60));
+    document.getElementById("dndin4").value = h4;
+    
+
+    let val5 = document.getElementById("din5").value;
+    let h5 = val5 * (Math.cos(80) * Math.cos(80));
+    document.getElementById("dndin5").value = h5;
+
+
+    let val6 = document.getElementById("din6").value;
+    let h6 = val6 * (Math.cos(100) * Math.cos(100));
+    document.getElementById("dndin6").value = h6;
+
+
+    let val7 = document.getElementById("din7").value;
+    let h7 = val7 * (Math.cos(120) * Math.cos(120));
+    document.getElementById("dndin7").value = h7;
+
+
+    let val8 = document.getElementById("din8").value;
+    let h8 = val8 * (Math.cos(140) * Math.cos(140));
+    document.getElementById("dndin8").value = h8;
+
+
+    let val9 = document.getElementById("din9").value;
+    let h9 = val9 * (Math.cos(160) * Math.cos(160));
+    document.getElementById("dndin9").value = h9;
+
+
+    let val0 = document.getElementById("din0").value;
+    let h0 = val9 * (Math.cos(180) * Math.cos(180));
+    document.getElementById("dndin0").value = h0;
+
+}
+
+function hd3() {
+    let val1 = document.getElementById("tin1").value;
+    let h1 = val1 * (Math.cos(0) * Math.cos(0));
+    document.getElementById("dntin1").value = h1;
+
+
+    let val2 = document.getElementById("tin2").value;
+    let h2 = val2 * (Math.cos(20) * Math.cos(20));
+    document.getElementById("dntin2").value = h2;
+
+    
+    let val3 = document.getElementById("tin3").value;
+    let h3 = val3 * (Math.cos(40) * Math.cos(40));
+    document.getElementById("dntin3").value = h3;
+
+
+    let val4 = document.getElementById("tin4").value;
+    let h4 = val4 * (Math.cos(60) * Math.cos(60));
+    document.getElementById("dntin4").value = h4;
+    
+
+    let val5 = document.getElementById("tin5").value;
+    let h5 = val5 * (Math.cos(80) * Math.cos(80));
+    document.getElementById("dntin5").value = h5;
+
+
+    let val6 = document.getElementById("tin6").value;
+    let h6 = val6 * (Math.cos(100) * Math.cos(100));
+    document.getElementById("dntin6").value = h6;
+
+
+    let val7 = document.getElementById("tin7").value;
+    let h7 = val7 * (Math.cos(120) * Math.cos(120));
+    document.getElementById("dntin7").value = h7;
+
+
+    let val8 = document.getElementById("tin8").value;
+    let h8 = val8 * (Math.cos(140) * Math.cos(140));
+    document.getElementById("dntin8").value = h8;
+
+
+    let val9 = document.getElementById("tin9").value;
+    let h9 = val9 * (Math.cos(160) * Math.cos(160));
+    document.getElementById("dntin9").value = h9;
+
+
+    let val0 = document.getElementById("tin0").value;
+    let h0 = val9 * (Math.cos(180) * Math.cos(180));
+    document.getElementById("dntin0").value = h0;
+
+}
+
+function hd4() {
+    let val1 = document.getElementById("emp1").value;
+    let h1 = val1 * (Math.cos(0) * Math.cos(0));
+    document.getElementById("dnemp1").value = h1;
+
+
+    let val2 = document.getElementById("emp2").value;
+    let h2 = val2 * (Math.cos(20) * Math.cos(20));
+    document.getElementById("dnemp2").value = h2;
+
+    
+    let val3 = document.getElementById("emp3").value;
+    let h3 = val3 * (Math.cos(40) * Math.cos(40));
+    document.getElementById("dnemp3").value = h3;
+
+
+    let val4 = document.getElementById("emp4").value;
+    let h4 = val4 * (Math.cos(60) * Math.cos(60));
+    document.getElementById("dnemp4").value = h4;
+    
+
+    let val5 = document.getElementById("emp5").value;
+    let h5 = val5 * (Math.cos(80) * Math.cos(80));
+    document.getElementById("dnemp5").value = h5;
+
+
+    let val6 = document.getElementById("emp6").value;
+    let h6 = val6 * (Math.cos(100) * Math.cos(100));
+    document.getElementById("dnemp6").value = h6;
+
+
+    let val7 = document.getElementById("emp7").value;
+    let h7 = val7 * (Math.cos(120) * Math.cos(120));
+    document.getElementById("dnemp7").value = h7;
+
+
+    let val8 = document.getElementById("emp8").value;
+    let h8 = val8 * (Math.cos(140) * Math.cos(140));
+    document.getElementById("dnemp8").value = h8;
+
+
+    let val9 = document.getElementById("emp9").value;
+    let h9 = val9 * (Math.cos(160) * Math.cos(160));
+    document.getElementById("dnemp9").value = h9;
+
+
+    let val0 = document.getElementById("emp0").value;
+    let h0 = val9 * (Math.cos(180) * Math.cos(180));
+    document.getElementById("dnemp0").value = h0;
+
+}
+
+
+
+
+
+function bijisegalanya() {
+    hd1();
+    hd2();
+    hd3();
+    hd4();
+}
